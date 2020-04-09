25011400167e94afd66384caa7f54b6a2079035d
Dominique <0931703@hr.nl>
2020-04-09 12:28:11 +0200

    changed  localadress in index.js

--- server/index.js
+++ server/index.js
@@ -66,9 +66,9 @@ var getIPAddresses = function () {
 };
 
 var udp = new osc.UDPPort({
-    localAddress: "178.62.209.37",
+    localAddress: "0.0.0.0",
     localPort: 5000,
-    remoteAddress: "178.62.209.37", // local 127.0.0.1 online server 178.62.209.37
+    remoteAddress: "127.0.0.1", // local 127.0.0.1 online server 178.62.209.37
     remotePort: 7500
 });
 
