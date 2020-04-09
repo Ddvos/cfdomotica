03dd6d95d8116ab92a5da3bde0fa3d86fe2c00db
Dominique <0931703@hr.nl>
2020-04-09 17:16:40 +0200

    changed remote address

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ var getIPAddresses = function () {
 var udp = new osc.UDPPort({
     localAddress: "178.62.209.37",
     localPort: 5000,
-    remoteAddress: "178.62.209.37", // local 127.0.0.1 online server 178.62.209.37
+    remoteAddress: "62.238.120.14", // local 127.0.0.1 online server 178.62.209.37
     remotePort: 7500
 });
 
