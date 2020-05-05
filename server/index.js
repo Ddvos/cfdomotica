b5a1126cbd80c548bb0e48410cfed6a75d4b742e
Dominique <0931703@hr.nl>
2020-05-05 20:25:41 +0200

    added webRTC livestream

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 //socket.io 
 var https = require('https').Server(app);
-var io = require('socket.io')(https);
+//var io = require('socket.io')(https);
 
 
 // middleware
@@ -45,43 +45,43 @@ mongoose.connection.on('connected',()=>{
 });
 
  //Socket.io stream
- io.on('connection', function (socket) {
+//  io.on('connection', function (socket) {
 
-    socket.on('broadcaster', function () {
-        //id of the broadcaster
-        broadcaster = socket.id;
-        socket.broadcast.emit('broadcaster');
-     });
-     //Default room
-    // Each Socket in Socket.IO is identified by a random, unguessable, unique identifier Socket#id. 
-     //For your convenience, each socket automatically joins a room identified by this id.
-     broadcaster =0;
-     socket.on('watcher', function () {
-        //tell to broadcast there is a watcher
-        broadcaster && socket.to(broadcaster).emit('watcher', socket.id);
-    });
+//     socket.on('broadcaster', function () {
+//         //id of the broadcaster
+//         broadcaster = socket.id;
+//         socket.broadcast.emit('broadcaster');
+//      });
+//      //Default room
+//     // Each Socket in Socket.IO is identified by a random, unguessable, unique identifier Socket#id. 
+//      //For your convenience, each socket automatically joins a room identified by this id.
+//      broadcaster =0;
+//      socket.on('watcher', function () {
+//         //tell to broadcast there is a watcher
+//         broadcaster && socket.to(broadcaster).emit('watcher', socket.id);
+//     });
  
-     //send sdp to the client
-     socket.on('offer', function (id /* of the watcher */, message) {
-        socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
-     });
-     //send sdp of the client to broad caster
-     socket.on('answer', function (id /* of the broadcaster */, message) {
-        socket.to(id).emit('answer', socket.id /* of the watcher */, message);
-     });
+//      //send sdp to the client
+//      socket.on('offer', function (id /* of the watcher */, message) {
+//         socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
+//      });
+//      //send sdp of the client to broad caster
+//      socket.on('answer', function (id /* of the broadcaster */, message) {
+//         socket.to(id).emit('answer', socket.id /* of the watcher */, message);
+//      });
  
-    //exchange ice candidate
-     socket.on('candidate', function (id, message) {
-        socket.to(id).emit('candidate', socket.id, message);
-     });
+//     //exchange ice candidate
+//      socket.on('candidate', function (id, message) {
+//         socket.to(id).emit('candidate', socket.id, message);
+//      });
  
-     socket.on('disconnect', function () {
-        broadcaster && socket.to(broadcaster).emit('bye', socket.id);
-     });
+//      socket.on('disconnect', function () {
+//         broadcaster && socket.to(broadcaster).emit('bye', socket.id);
+//      });
  
-  });
+//   });
 
-  http.listen(4000, function () {
+  https.listen(4000, function () {
      console.log('WebRTC socket.io is listining on port: 4000');
   });
  
