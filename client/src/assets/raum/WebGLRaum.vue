ed6892a7a72c1c21b4faf6f524dbdf0f6e4bf8b8
Dominique <0931703@hr.nl>
2020-08-04 17:38:12 +0200

    added OSC support

--- client/src/assets/raum/WebGLRaum.vue
+++ client/src/assets/raum/WebGLRaum.vue
@@ -36,15 +36,16 @@ export default {
        bigBall: null,
       smallBall: null,
       //colorvlakken
-      vlak1color1: '#de3c31',
-      vlak1color2: '#42f5b3',
-      vlak2color2: '#6ca18d',
-      vlak3color1: '#5c4909',
-      vlak3color2: '#de3c31',
-      vlak4color1: '#de3c31',
-      vlak4color2: '#31c7de',
+      colorVlak1: ['rgb(222, 60, 49)','rgb(13, 212, 209)',0.5], // kleuren en positie van vlakken
+      colorVlak2: ['rgb(222, 60, 49)','rgb(13, 212, 209)',0.5], // kleuren en positie van vlakken
+      colorVlak3: ['rgb(222, 60, 49)','rgb(13, 212, 209)',0.5], // kleuren en positie van vlakken
+      colorVlak4: ['rgb(222, 60, 49)','rgb(13, 212, 209)',0.5], // kleuren en positie van vlakken
+      // breedite driehoeken
+      width: null,
       // detection
       mesh1Detection: null,
+      mesh2Detection: null,
+      vlak1Position: null,
 
    }
   },
@@ -52,7 +53,7 @@ export default {
   
   },
   created() {
-    
+     this.OSCMessage(); 
     },
   mounted(){
    // this.Three = window.Three = require('Three');
@@ -62,6 +63,33 @@ export default {
 
     },
   methods:{
+    OSCMessage: function(){        
+        port.on("message", (oscMessage) => {
+             this.OSCMessages(oscMessage);
+             //console.log(oscMessage);
+        });
+     },
+      OSCMessages: function(oscMessage){ 
+            // put incoming OSC data in colorvlakken
+             if(oscMessage.address == "/color_pole1" ){
+                 this.colorVlak1 = ['rgb('+oscMessage.args[0]+','+oscMessage.args[1]+','+oscMessage.args[2]+')','rgb('+oscMessage.args[3]+','+oscMessage.args[4]+','+oscMessage.args[5]+')',oscMessage.args[6]]//twee waardes rgb(255, 0, 0) 
+               
+                 this.mesh1.material.uniforms.vlak1color1.value = new this.$three.Color(this.colorVlak1[0])
+                 this.mesh1.material.uniforms.vlak1color2.value = new this.$three.Color(this.colorVlak1[1])
+                 this.mesh1.material.uniforms.positionVlak1.value = (this.width)/1*this.colorVlak1[2]
+
+                 this.colorVlak2 = ['rgb('+oscMessage.args[7]+','+oscMessage.args[8]+','+oscMessage.args[9]+')','rgb('+oscMessage.args[10]+','+oscMessage.args[11]+','+oscMessage.args[12]+')',oscMessage.args[13]]//twee waardes rgb(255, 0, 0) 
+                 //console.log(this.colorVlak2);
+                 this.mesh2.material.uniforms.vlak2color1.value = new this.$three.Color(this.colorVlak2[0])
+                 this.mesh2.material.uniforms.vlak2color2.value = new this.$three.Color(this.colorVlak2[1])
+                 this.mesh2.material.uniforms.positionVlak2.value = (this.width)/1*this.colorVlak2[2]
+                 }
+            
+                 
+              
+                  
+                 
+     },
     
 
     init: function() {
@@ -78,13 +106,13 @@ export default {
         //camera
         this.camera = new this.$three.PerspectiveCamera(100, container.clientWidth/container.clientHeight, 0.1, 100);
         this.camera.position.z = 10;
-console.log(container.clientWidth)
+       //console.log(container.clientWidth)
         //scene
         this.scene = new this.$three.Scene();
         
 
          var ground = new this.$three.BoxGeometry( 20,20, 0 );
-         var Color1Geometry = new this.$three.Geometry();
+         var Color1Geometry = new this.$three.Geometry();  // color vlakken 
          var Color2Geometry = new this.$three.Geometry();
          var Color3Geometry = new this.$three.Geometry();
          var Color4Geometry = new this.$three.Geometry();
@@ -179,14 +207,23 @@ console.log(container.clientWidth)
             // pilaar kleur 
           //var material = new this.$three.MeshBasicMaterial({ vertexColors: this.$three.VertexColors, side: this.$three.DoubleSide});  //vertexColors: this.$three.FaceColors, side: this.$three.DoubleSide 
 
-          
+
+          // calculation height and width triangle for dynamic blur in procent % 0 to 1
+         console.log()
+
+          this.width =Color1Geometry.vertices[1].y-Color1Geometry.vertices[0].y
+          var vlak1Position = (this.width)/1*this.colorVlak1[2]
+          var vlak2Position = (this.width)/1*this.colorVlak2[2]
+          var vlak3Position = (this.width)/1*this.colorVlak3[2]
+          var vlak4Position = (this.width)/1*this.colorVlak4[2]
+            
+          //Color1Geometry.vertices[0].y
           /// test vertexshader en fragmentshader
       var custom1Material = new this.$three.ShaderMaterial({
                  uniforms: {
-                   vlak1color1: { value: new this.$three.Color(this.vlak1color1)},
-                   vlak1color2: { value: new this.$three.Color(this.vlak1color2)},
-                   positionVlak1: {value: 1.0},
-                  // lightRadius: {value: 0.8},
+                   vlak1color1: { value: new this.$three.Color(this.colorVlak1[0])},
+                   vlak1color2: { value: new this.$three.Color(this.colorVlak1[1])},
+                   positionVlak1: {value: vlak1Position.toFixed(2)},
                  },
                  vertexShader: `
                 
@@ -211,8 +248,8 @@ console.log(container.clientWidth)
                     
                        float alpha = smoothstep(2.0, 0.0, vUv.y-2.0);
 
-                        // y < 1 = color1, > 2 = color2
-                        float colorMix = smoothstep(1.0, 2.0, vUv.y);
+                        // y < 1 = color1, > 2 = color2   // positie tussen boven en onderkant kleur mix
+                        float colorMix = smoothstep(0.0, 2.0, vUv.y-positionVlak1); //positionVlak1
 
                         gl_FragColor = vec4(mix(vlak1color1, vlak1color2, colorMix), alpha);
                    }
@@ -221,9 +258,9 @@ console.log(container.clientWidth)
                /// vertexshader en fragmentshader rechterkant
          var custom2Material = new this.$three.ShaderMaterial({
                  uniforms: {
-                   vlak2color1: { value: new this.$three.Color(this.vlak2color1)},
-                   vlak2color2: {type: 'vec2', value: new this.$three.Color(this.vlak2color2)},
-                   positionVlak2: {value: 1.0},
+                   vlak2color1: { value: new this.$three.Color(this.colorVlak2[0])},
+                   vlak2color2: { value: new this.$three.Color(this.colorVlak2[1])},
+                   positionVlak2: {value: vlak2Position.toFixed(2)},
                  },
                  vertexShader: `
                 
@@ -231,8 +268,7 @@ console.log(container.clientWidth)
 
                    void main() {
                      vUv = position; 
-
-                  
+                 
                      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
                    }
                  `,
@@ -241,15 +277,14 @@ console.log(container.clientWidth)
                   uniform vec3 vlak2color1;
                    uniform vec3 vlak2color2;
                    uniform float positionVlak2;
-
-                
+               
                    varying vec3 vUv;
                   
                    void main() {     
-                      float alpha = smoothstep(2.0, 0.0, vUv.x-1.0);
+                      float alpha = smoothstep(2.0, 0.0, vUv.x-2.0);
 
                         // y < 1 = color1, > 2 = color2
-                        float colorMix = smoothstep(1.0, 2.0, vUv.x+0.5);
+                        float colorMix = smoothstep(0.0, 2.0, vUv.x-positionVlak2);
 
                         gl_FragColor = vec4(mix(vlak2color1, vlak2color2, colorMix), alpha);
                    }
@@ -258,9 +293,9 @@ console.log(container.clientWidth)
                 /// vertexshader en fragmentshaderonderkant
                var custom3Material = new this.$three.ShaderMaterial({
                  uniforms: {
-                   vlak3color1: { value: new this.$three.Color('#31c7de')},
-                   vlak3color2: {type: 'vec2', value: new this.$three.Color('#de3c31')},
-                   positionVlak3: {value: -3.5},
+                   vlak3color1: { value: new this.$three.Color(this.colorVlak3[0])},
+                   vlak3color2: { value: new this.$three.Color(this.colorVlak3[1])},
+                   positionVlak3: {value: vlak3Position.toFixed(2)},
                  },
                  vertexShader: `
                 
@@ -268,7 +303,6 @@ console.log(container.clientWidth)
 
                    void main() {
                      vUv = position; 
-
                   
                      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
                    }
@@ -278,27 +312,26 @@ console.log(container.clientWidth)
                   uniform vec3 vlak3color1;
                    uniform vec3 vlak3color2;
                    uniform float positionVlak3;
-
-                
+              
                    varying vec3 vUv;
                   
                    void main() {     
                     
-                      float alpha = smoothstep(0.0, 2.0, vUv.y+3.5);
+                      float alpha = smoothstep(0.0, 2.0, vUv.y+4.0);
 
                         // y < 1 = color1, > 2 = color2
-                        float colorMix = smoothstep(1.0, 2.0, vUv.y+4.0);
+                        float colorMix = smoothstep(0.0, 2.0, vUv.y+2.0+positionVlak3); //2.0 voor de afwijking 
 
-                        gl_FragColor = vec4(mix(vlak3color1, vlak3color2, colorMix), alpha);
+                        gl_FragColor = vec4(mix(vlak3color2, vlak3color1, colorMix), alpha);
                    }
                    `,transparent: true,
               });
                /// vertexshader en fragmentshaderonderkant
                var custom4Material = new this.$three.ShaderMaterial({
                  uniforms: {
-                   vlak4color1: { value: new this.$three.Color('#31c7de')},
-                   vlak4color2: {type: 'vec2', value: new this.$three.Color('#de3c31')},
-                   positionVlak4: {value: -4},
+                   vlak4color1: { value: new this.$three.Color(this.colorVlak4[0])},
+                   vlak4color2: { value: new this.$three.Color(this.colorVlak4[1])},
+                   positionVlak4: {value: vlak4Position.toFixed(2)},
                  },
                  vertexShader: `
                 
@@ -306,7 +339,6 @@ console.log(container.clientWidth)
 
                    void main() {
                      vUv = position; 
-
                   
                      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
                    }
@@ -316,8 +348,7 @@ console.log(container.clientWidth)
                   uniform vec3 vlak4color1;
                    uniform vec3 vlak4color2;
                    uniform float positionVlak4;
-
-                
+              
                    varying vec3 vUv;
                   
                    void main() {     
@@ -325,22 +356,26 @@ console.log(container.clientWidth)
                        float alpha = smoothstep(0.0, 2.0, vUv.x+4.0);
 
                         // y < 1 = color1, > 2 = color2
-                        float colorMix = smoothstep(0.0, 2.0, vUv.x+3.0);
+                        float colorMix = smoothstep(0.0, 2.0, vUv.x+2.0+positionVlak4); // +2.0 voor de afwijking en + de postie
 
-                        gl_FragColor = vec4(mix(vlak4color1, vlak4color2, colorMix), alpha);
+                        gl_FragColor = vec4(mix(vlak4color2, vlak4color1, colorMix), alpha);
                    }
                    `,transparent: true,
-              });
-         
+              });        
 
          /// einde test vertexshader en fragmentshade
 
          //detection material/field
-        var materialDetection = new this.$three.MeshLambertMaterial( {color: 0xffff00, transparent: true, opacity: 0.0   } );
-        
+        var materialDetection = new this.$three.MeshLambertMaterial( {color: 0xffff00, transparent: true, opacity: 0.5   } );        
      
         this.mesh1Detection = new this.$three.Mesh( detection1, materialDetection );
-          this.mesh1Detection.position.y = 3;
+        this.mesh1Detection.position.y = 3;
+
+        this.mesh2Detection = new this.$three.Mesh( detection1, materialDetection );
+         this.mesh2Detection.position.x = 3;
+         this.mesh2Detection.rotation.z = ( Math.PI / 2 );
+    
+        //this.mesh2Detection.rotation.x = Math.PI / 2 
         
 
         this.mesh1 = new this.$three.Mesh( Color1Geometry,  custom1Material );
@@ -362,16 +397,13 @@ console.log(container.clientWidth)
           this.groundMesh = new this.$three.Mesh( ground, groundMaterial );
           this.groundMesh.position.set(0, 0, -1)
           //this.groundMesh.rotation.x += -0.2
-          //this.groundMesh.rotation.y += 3.1
-         
+          //this.groundMesh.rotation.y += 3.1        
 
           // mouse
           var mouseMaterial = new  this.$three.MeshBasicMaterial( { color: 0xffff00 } );
-         this.mouseMesh = new  this.$three.Mesh( mouseGeometry, mouseMaterial );
-          
-           
-
-          this.scene.add(this.mesh1,this.mesh2,this.mesh3, this.mesh4,this.groundMesh,this.meshPilaar,this.mouseMesh,this.mesh1Detection);
+          this.mouseMesh = new  this.$three.Mesh( mouseGeometry, mouseMaterial );
+               
+          this.scene.add(this.mesh1,this.mesh2,this.mesh3, this.mesh4,this.groundMesh,this.meshPilaar,this.mouseMesh,this.mesh2Detection);
 
   
     },
@@ -401,19 +433,23 @@ console.log(container.clientWidth)
           //collision detection ball with vlak1
            var mesh1Collision = new this.$three.Box3(new this.$three.Vector3(), new this.$three.Vector3());
                mesh1Collision.setFromObject(this.mesh1Detection);
-
             
-              // console.log(  testmesh.intersectsTriangle(testmesh))
-               
-          var mouseCollision = new this.$three.Box3(new this.$three.Vector3(), new this.$three.Vector3());
+          // console.log(  testmesh.intersectsTriangle(testmesh))
+           var mouseCollision = new this.$three.Box3(new this.$three.Vector3(), new this.$three.Vector3());
                mouseCollision.setFromObject(this.mouseMesh)
               //console.log(  testmesh)
                if(mouseCollision.intersectsBox( mesh1Collision)){
-                  this.mesh1.material.uniforms.vlak1color2.value = new this.$three.Color('#0a3df5')
-                  
+                   // console.log("side1 pole1 active")
+                   port.send({
+                        address: "/pole1_1",
+                        args:  [1]
+                    });                
                }
                else{
-                 this.mesh1.material.uniforms.vlak1color2.value = new this.$three.Color('#6ca18d')
+                    port.send({
+                        address: "/pole1_1",
+                        args:  [0]
+                    });
                }
             //console.log(  mouseCollision.isIntersectionBox(mesh1Collision))
           }
