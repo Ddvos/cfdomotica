fc5f058a4eba8a13e58ffa063d4cf9da7b3cc7bd
ddvos <0931703@hr.nl>
2020-11-12 16:23:17 +0100

    made some changes to mongodb

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -6,30 +6,36 @@ const router = express.Router();
 
 const mongodb_URI = 'mongodb+srv://Circus_Family:mYIz6bPl1ZRfhbtF@circusfamily.vehzf.mongodb.net/circusfamily?retryWrites=true&w=majority' //'mongodb+srv://CircusFamily:mYIz6bPl1ZRfhbtF@cluster0-7znii.mongodb.net/test?retryWrites=true&w=majority'
 
+ 
+mongoose.connect(mongodb_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected',()=>{
+    console.log('MonogDB is connected with online database');
+});
+
+mongoose.connection.on('error',(error)=>{
+    console.log('MonogDB error'+error);
+});
+
+mongoose.model('posts',{name: String});
 //Get Posts
 router.get('/', async (req,res)=>{
     //res.send('hello')
-     const posts = await loadPostsCollection();
-     res.send(await posts.find({}).toArray());
+    mongoose.model('posts').find(function(err, posts){
+        res.send( posts);
+    });
+    
+  
 });
 
 //Add Posts
 
 // Delete Posts
 
- async function loadPostsCollection(){
-    const client = await mongoose.connect(mongodb_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
 
-    mongoose.connection.on('connected',()=>{
-        console.log('MonogDB is connected with online database');
-    });
-
-    mongoose.connection.on('error',(error)=>{
-        console.log('MonogDB error'+error);
-    });
-     return client;
- }
+   
+ 
 module.exports = router;
