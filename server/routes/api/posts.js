5bc1c72153ea33c4660df229429e2a1583c15eb1
ddvos <0931703@hr.nl>
2020-11-12 14:51:32 +0100

    made some changes to mongodb

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const mongodb = require('mongodb');
+//const mongodb = require('mongodb');
 
 const router = express.Router();
 
 //Get Posts
-router.get('/', async (req,res)=>{
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}).toArray());
+router.get('/',(req,res)=>{
+    // const posts = await loadPostsCollection();
+    // res.send(await posts.find({}).toArray());
 });
 
 //Add Posts
@@ -14,11 +14,11 @@ router.get('/', async (req,res)=>{
 // Delete Posts
 
 
-async function loadPostsCollection(){
-    const client = await mongodb.MongoClient('mongodb+srv://Circus_Family:mYIz6bPl1ZRfhbtF@circusfamily.vehzf.mongodb.net/circusfamily?retryWrites=true&w=majority',{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    return client.db('circusfamily').collection('posts');
-}
+// async function loadPostsCollection(){
+//     const client = await mongodb.MongoClient('mongodb+srv://Circus_Family:mYIz6bPl1ZRfhbtF@circusfamily.vehzf.mongodb.net/circusfamily?retryWrites=true&w=majority',{
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//     return client.db('circusfamily').collection('posts');
+// }
 module.exports = router;
